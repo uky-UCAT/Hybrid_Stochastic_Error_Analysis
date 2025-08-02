f04af9eac3045831f946964807e9480494caa6f4
ionep <tbipin12@gmail.com>
2025-08-02 14:48:56 -0400

    Fixed 3 bits and 4 bits different multiplication

--- ErrorSplitMultiply.py
+++ ErrorSplitMultiply.py
@@ -26,10 +26,15 @@ For example, if the operation is ADD for a combination such as (2,1) -> result =
 """
 
 
-bitwidth = 4
+bitwidth = 8
+
+# for sign bit included in 8 bits
 operand1_range = torch.arange(0, 2**(bitwidth-1))
-operand2_range = torch.arange(0, 2**bitwidth)
-# print(operand1_range)
+operand2_range = torch.arange(0, 2**(bitwidth-1))
+
+# for separate sign bit
+# operand1_range = torch.arange(0, 2**(bitwidth))
+# operand2_range = torch.arange(0, 2**(bitwidth))
 operation = "MUL"
 
 
@@ -38,37 +43,46 @@ for operand1 in operand1_range:
     for operand2 in operand2_range:
         result = {}
         # * Decimal output computation
-        # operand1 = torch.tensor(155)
-        # operand2 = torch.tensor(132)
 
         output = operand1 * operand2
         
         # * Stochastic Output computation
+        
+        #convert the operands to binary
         operand1_b = getDecimalToBinary(operand1, bitwidth)
         operand2_b = getDecimalToBinary(operand2, bitwidth)
         
+        #split binary into MSB and LSB
         operand1_msb = getBinaryToDecimal(operand1_b[: bitwidth // 2],bitwidth//2)
         operand1_lsb = getBinaryToDecimal(operand1_b[bitwidth // 2 :],bitwidth//2)
-        
+
         operand2_msb = getBinaryToDecimal(operand2_b[: bitwidth // 2],bitwidth//2)
         operand2_lsb = getBinaryToDecimal(operand2_b[bitwidth // 2 :],bitwidth//2)
         
+        # convert operand1's MSB and LSB parts to TCU (zero padded here in software but is actually padded after conversion and result is same)
         operand1_msb_sc = getDecimalToUnary(operand1_msb, bitwidth//2)
         operand1_lsb_sc = getDecimalToUnary(operand1_lsb, bitwidth//2)
         
-        # operand2_msb_sc = getDecimalToUnaryMul(abs(operand2_msb), bitwidth//2)
-        # operand2_lsb_sc = getDecimalToUnaryMul(abs(operand2_lsb), bitwidth//2)
-        operand2_msb_sc = getDecimalToUnary(abs(operand2_msb), bitwidth//2)
-        operand2_lsb_sc = getDecimalToUnary(abs(operand2_lsb), bitwidth//2)
+        # pass operand2's MSB and LSB through the bit shifting network after TCU conversion
+        operand2_msb_sc = getDecimalToUnaryMul(abs(operand2_msb), bitwidth//2)
+        operand2_lsb_sc = getDecimalToUnaryMul(abs(operand2_lsb), bitwidth//2)
+    
+        # 3 bits of the MSBs to get 8 bits SC
+        operand1_msb_sc_1 = getDecimalToUnary(operand1_msb, bitwidth//2-1) # this is same as taking last 8 bits from operand1_msb_sc
+        operand2_msb_sc_1 = getDecimalToUnaryMul(abs(operand2_msb), bitwidth//2-1)
         
-        p1 = stochasticMUL(operand1_msb_sc, operand2_msb_sc)
+        # M1.M2 is 8 bits multiplication
+        p1 = stochasticMUL(operand1_msb_sc_1, operand2_msb_sc_1)
+        
+        # M1.L2 is 16 bits multiplication where M1 is padded with 8 zeros in the front (in TCU all bits are in the rightmost so it doesnt affect)
         p2 = stochasticMUL(operand1_msb_sc, operand2_lsb_sc)
+        
+        # L1.M2 is 16 bits where M2 is zero padded from 3 bits to 4 bits before SC conversion
         p3 = stochasticMUL(operand1_lsb_sc, operand2_msb_sc)
+        
+        # L1.L2 is 16 bits
         p4 = stochasticMUL(operand1_lsb_sc, operand2_lsb_sc)
         
-        # output_sc_bs = stochasticMUL(operand1_sc, operand2_sc)
-        
-        # output_sc = countOneInBS(p1) * 2**bitwidth + countOneInBS(p2) * 2**(bitwidth // 2) + countOneInBS(p3) * 2**(bitwidth // 2) + countOneInBS(p4)
         # output_sc = countOneInBS(p1) * 2**(bitwidth // 2) + countOneInBS(p2) + countOneInBS(p3)  + countOneInBS(p4) / 2 ** (bitwidth//2)
         output_sc = countOneInBS(p1) * 2**(bitwidth // 2) + countOneInBS(p2) + countOneInBS(p3) 
         
@@ -80,17 +94,13 @@ for operand1 in operand1_range:
         result["SC_OP1"] = np.array(operand1_sc)
         result["SC_OP2"] = np.array(operand2_sc)
         result["Decimal_OPT_Scaled"] = output.numpy()/2**bitwidth
-        # result["SC_OPT_BS"] = output_sc_bs.numpy()
         result["SC_OPT"] = output_sc.numpy()
         result["Error"] = abs(
             ((result["Decimal_OP1"] / 2**bitwidth) * (result["Decimal_OP2"] / 2**bitwidth))
             - ((result["SC_OPT"] / 2**bitwidth))
         )
         result_list.append(result)
-        # print("Direct:"+str(result['Decimal_OPT']/2**bitwidth))
-        # print("Result:"+str(result['SC_OPT']))
-    #     break
-    # break
+
 mae = np.mean([abs(r["Error"]) for r in result_list])
 print("Mean Absolute Error: ", mae)
 
